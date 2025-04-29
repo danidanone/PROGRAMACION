44f97530ee32be6c490aaa2f58129c4d07241e56
danidanone <56256497+danidanone@users.noreply.github.com>
2025-04-29 18:54:48 +0200

    Add files via upload

---
+++ GestionDeFicheros/src/model/Funciones.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Funciones {
+
+    public static void createFolder(String folderName) {
+        // ...
+    }
+
+    public static void createFile(String path, String fileName, String content) {
+        // ...
+    }
+
+    public static String[] showListFiles(String path) {
+        // ...
+        return null;
+    }
+
+    public static String showFile(String path, String fileName) {
+        // ...
+        return null;
+    }
+
+    public static boolean overWriteFile(String path, String fileName, String newContent) {
+        // ...
+        return false;
+    }
+
+    public static void deleteFile(String path, String fileName) {
+        // ...
+    }
+
+    public static int countChars(String path, String fileName) {
+        // ...
+        return 0;
+    }
+
+    public static int countWords(String path, String fileName) {
+        // ...
+        return 0;
+    }
+
+    public static String swapWords(String path, String fileName, String oldWord, String newWord) {
+        // ...
+        return null;
+    }
+
+    public static void printPDF(String path, String fileName) {
+        // ...
+    }
+}
---
+++ GestionDeFicheros/src/view/console/Main.java
@@ -0,0 +1,7 @@
+package view.console;
+
+public class Main {
+    public static void main(String[] args) {
+        // Aquí irá el menú con do-while
+    }
+}
